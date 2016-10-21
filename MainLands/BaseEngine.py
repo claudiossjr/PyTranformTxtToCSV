897df0b5b707a0fc057ad431d233154bf479049c
dinhujr <c.souza0804@gmail.com>
2016-10-20 22:44:17 -0200

    formatting

--- MainLands/BaseEngine.py
+++ MainLands/BaseEngine.py
@@ -2,7 +2,9 @@ __author__ = 'claudio'
 from pathlib import Path
 from Objects import NodeInfo
 
+
 class TransformEngine:
+
     def __init__(self, destination, source, folder):
         self.__destination_node__ = destination
         self.__source_node__ = source
@@ -14,6 +16,9 @@ class TransformEngine:
         self.__build_sourceInfo__()
 
     def __parse_dest_and_sour_file(self, file_name):
+        """
+        This function opens destination and source node file in order to fill node information dictionary
+        """
         file_reader = open(file_name, "r")
         # temp_path = str.format("{0}/{1}", self.__main_folder__, "exitFile.txt")
         # file_writer = open(temp_path, "w+")
@@ -29,6 +34,9 @@ class TransformEngine:
         file_reader.close()
 
     def __build_sourceInfo__(self):
+        """
+        This function build dictionary that will be used to get lat/long node information
+        """
         # Reading destination file
         self.__parse_dest_and_sour_file(self.__destination_node__)
 
--- MainLands/Main.py
+++ MainLands/Main.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
 __author__ = 'claudio'
-import sys,getopt
+import sys, getopt
 from BaseEngine import TransformEngine
 
 
-def parseOptions(argv):
+def parse_options(args):
     try:
-        opts, args = getopt.getopt(argv,"hd:s:f:", ["destination=", "source=", "folder="])
+        opts, argv = getopt.getopt(args, "hd:s:f:", ["destination=", "source=", "folder="])
     except getopt.GetoptError:
         print("Main.py -dest <destinationNode> -sour <SourceNode> -fol <filesFolder>")
-    optionsDict = {}
+    options_dict = {}
     for opt, arg in opts:
         if opt == "-h":
             print("Main.py -d <destinationNode> -s <SourceNode> -f <filesFolder>\n"
                   "Main.py --destination <destinationNode> --source <SourceNode> --folder <filesFolder>")
             sys.exit(2)
         elif opt in ("-d", "--destination"):
-            optionsDict["destination"] = arg
+            options_dict["destination"] = arg
         elif opt in ("-s", "--source"):
-            optionsDict["source"] = arg
+            options_dict["source"] = arg
         elif opt in ("-f", "--folder"):
-            optionsDict["folder"] = arg
+            options_dict["folder"] = arg
 
-    return optionsDict
+    return options_dict
+
 
 def main(args):
-    options = parseOptions(args)
+    options = parse_options(args)
     print(options)
-    destinationFile = options["destination"]
-    sourceFile = options["source"]
-    filesFolder = options["folder"]
-    engine = TransformEngine(destinationFile, sourceFile, filesFolder)
+    destination_file = options["destination"]
+    source_file = options["source"]
+    files_folder = options["folder"]
+    engine = TransformEngine(destination_file, source_file, files_folder)
     engine.run()
-    print("Hello,  World!")
+
     print(args)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
